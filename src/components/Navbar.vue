5d7b3537b17e4e5ad34e7cc70506f1f0ea6e54fd
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2025-02-12 22:51:54 -0300

    Aplicando o Vue Router, matendo sempre o footer e o vue nas páginas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" alt="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/Pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>   
+    export default{
+        name:"Navbar",
+
+        props:{
+
+        },
+
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>home</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script>{
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
