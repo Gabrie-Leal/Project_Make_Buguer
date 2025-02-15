4f2b9f9acda94c1f4a47e0d1533b22bddfe87a68
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2025-02-15 18:52:40 -0300

    Resgatando os dados do bd e inserindo no formulário

--- src/components/FormularioBurguer.vue
+++ src/components/FormularioBurguer.vue
@@ -4,39 +4,46 @@
         <form id="burguer-form">
             <div class="input-container">
                 <label for="nome">Nome do cliente:</label>
-                <input type="text" name="nome" id="nome" v-model="nome" placeholder="Digite seu nome">
+                <input type="text" name="nome" id="nome" v-model="nomeCliente" placeholder="Digite seu nome">
             </div>
 
             <div class="input-container">
                 <label for="pao">Escolha o tipo de pão:</label>
                 <select name="pao" id="pao" v-model="pao">
                     <option value="">Selecione o seu pão</option>
-                    <option value="Integral">Integral</option>
+
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                        {{ pao.tipo }}
+                    </option>
+                    <!--rodando o paes cada elemento chama pao
+                    ket é id pq o json tem id, caso n tivesse usava o index msm
+                    pao.tipo é pq temos o tipo de pao
+                    so olhar o json q vc entede
+                    -->
                 </select>
+
             </div>
 
             <div class="input-container">
                 <label for="carne">Escolha o tipo carne:</label>
                 <select name="carne" id="carne" v-model="carne">
-                    <option value="">selecione o tipo de carne</option>
-                    <option value="Frango">Frango</option>
+                    <option value="">Selecione o tipo de carne</option>
+
+                    <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                        {{ carne.tipo }}
+                    </option>
+
                 </select>
             </div>
             
             <div id="opcionais-container" class="input-container">
-                <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opicionais" value="salame">
-                    <span>Salame</span>
+                <label id="opcionais-title" fora="opcionais">Selecione os opcionais:</label>
+                
+                <div class="checkbox-container" v-for="opcional in opcionaisData" :key="opcional.id">
+                    <input type="checkbox" name="opcionais" v-model="opicionais" value="opcional.tipo">
+                    <span>{{ opcional.tipo }}</span>
                 </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opicionais" value="salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opicionais" value="salame">
-                    <span>Salame</span>
-                </div>
+                
             </div>
 
             <div class="input-container">
@@ -51,6 +58,38 @@
 <script>
     export default{
         name:'FormularioBurguer',
+
+        data(){
+            return{
+                //vem do servidor
+                paes:null,
+                carnes:null,
+                opcionaisData:null,
+
+                //enviados, preenchido no formulario
+                nomeCliente:null,
+                pao:null,
+                carne:null,
+                opcionais:[],
+                status:"Solicitado",
+                msg:null,
+            }
+        },
+
+        methods:{
+            async getIngredientes(){
+                const requisicao = await fetch("http://localhost:3000/ingredientes");
+                const data = await requisicao.json();//espera a requisicao, espera transformar p/ json
+                console.log(data);
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionaisData = data.opcionais;
+            },
+        },
+
+        mounted(){
+            this.getIngredientes();
+        },
         
     }
 </script>
@@ -67,7 +106,6 @@
 
     .input-container{
         display: flex;
-        justify-content: center;
         flex-direction: column;
         margin-bottom: 20px;
     }
@@ -94,11 +132,10 @@
         width:298px;
     }
 
-    .checkbox-container{
+    .checkbox-container {
         display: flex;
-        justify-content: center;
         align-items: flex-start;
-        width:50%;
+        width: 50%;
         margin-bottom: 20px;
     }
 
